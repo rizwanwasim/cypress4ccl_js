1afb25de848d56b5f41325554a64140c66ce2cd2
rizwanwasim <rizwanwasim115@gmail.com>
2023-05-25 22:50:28 +0500

    api new

---
+++ cypress/e2e/features/logintry.feature
@@ -0,0 +1,16 @@
+Feature: API Testing
+
+ 
+Background:
+    Given A base Url for API Test
+
+Scenario: Login Try
+    Given A base Url for API Test
+    When We request the Post Login API
+    Then The response status code should be 200
+    And Token will be in response
+
+Scenario: Login Try with wrong details
+    When We request the Post Login API with Wrong details
+    # Then The response status code should be 400
+    And Error message will be shown
---
+++ cypress/e2e/step_definitions/logintry.js
@@ -0,0 +1,28 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+
+When("We request the Post Login API", () => {
+    cy.request('POST',"/api/login", {"email": "eve.holt@reqres.in", "password": "cityslicka"}).as('response');
+  });
+
+//   Then("The response status code should be 200", () => {
+//     cy.get('@response').its('status').should('eq', 200);
+//   });
+
+  Then('Token will be in response', () => {
+    cy.get('@response').its('body').should("have.a.property", "token")
+  });
+
+When("We request the Post Login API with Wrong details", () => {
+    cy.request({url:"/api/login", "email": "peter@klaven", failOnStatusCode: false}).as('response');
+  });
+
+Then("The response status code should be 400", () => {
+    cy.get('@response').its('status').should('eq', 400);
+  });
+
+
+Then("Error message will be shown", () => {
+    cy.get('@response').should("have.property", "error")
+  });
